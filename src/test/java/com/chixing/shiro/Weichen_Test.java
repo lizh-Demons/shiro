6705f5f1ba7264f428343749908cabe2c6f131aa
aoguanshan <xunmo_li@163.com>
2020-12-02 20:10:32 +0800

    weiChen first commit for WeiChen_Test.java

---
+++ src/test/java/com/chixing/shiro/Weichen_Test.java
@@ -0,0 +1,12 @@
+package com.chixing.shiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Weichen_Test {
+    @Test
+    void test01(){
+        System.out.println("hello Mr.hua world!");
+    }
+}
